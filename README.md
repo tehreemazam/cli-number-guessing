c49e10619947e735978a7c1d0cdf9bed15d3ab9f
tehreemazam <164249734+tehreemazam@users.noreply.github.com>
2024-03-26 23:43:57 +0500

    created number guessing game

---
+++ README.md
@@ -0,0 +1,1 @@
+tehreem-cli-number-guessing
---
+++ index.js
@@ -0,0 +1,17 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userguessednumber",
+        type: "number",
+        message: "please guess the number 1-10 : ",
+    }
+]);
+console.log(answer);
+if (answer.userguessednumber === randomNumber) {
+    console.log("Congratulations! you guessed a right number");
+}
+else {
+    console.log("you guessed a wrong number");
+}
